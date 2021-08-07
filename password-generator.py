d7f6669992a6ce0d7e43abe91213fc21a5140c62
Kieron <17520284+KieronJenkins@users.noreply.github.com>
2021-08-07 22:05:18 +0100

    Updated password-generator.py file
    Added comments

--- password-generator.py
+++ password-generator.py
@@ -1,17 +1,17 @@
 import random
 
-password_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%&*@#^[]{}~()<>=+:;/"
-password_amount = input("How many characters do you want your password to have:\n")
-password_amount = int(password_amount)
+password_characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!£$%&*@#^[]{}~()<>=+:;/" #Characters being used for password
+password_amount = input("How many characters do you want your password to have:\n") 
+password_amount = int(password_amount) #User types the length they want the password to be integers only
 
 
 def password_gen():
     for i in range(password_amount):
-        user_password = ""
+        user_password = "" #Shell for password
         for p in range(password_amount):
-            user_password += random.choice(password_characters)
+            user_password += random.choice(password_characters) #Randoms between the characters in password_characters above
 
-    print(f"Password: {user_password}")
+    print(f"Password: {user_password}") #Prints the generated password
 
 
-password_gen()
+password_gen() #Runs the above function
